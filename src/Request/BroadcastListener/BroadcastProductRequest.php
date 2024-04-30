b27daa5402b891af59f5a62789d73554aa3d1d81
Hung Nguyen <mizzu198x@gmail.com>
2024-04-30 22:59:21 +0700

    Refactor

--- src/Request/BroadcastListener/BroadcastProductRequest.php
+++ src/Request/BroadcastListener/BroadcastProductRequest.php
@@ -8,7 +8,6 @@ use Symfony\Sample\Request\BroadcastListener\Product\ListPrice;
 use Symfony\Sample\ContextualInterface;
 use Symfony\Sample\RequestBodyInterface;
 use Symfony\Sample\ValidatableRequestInterface;
-use JMS\Serializer\Annotation as JMS;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class BroadcastProductRequest implements
@@ -16,45 +15,35 @@ class BroadcastProductRequest implements
     ValidatableRequestInterface,
     ContextualInterface
 {
-    #[JMS\Type(name: 'string')]
     #[Assert\Type(type: 'string')]
     #[Assert\Length(min: 1, max: 30)]
     #[Assert\NotBlank]
     public string $sku;
 
-    #[JMS\Type(name: 'string')]
     #[Assert\NotNull]
     #[Assert\Type(type: 'string')]
     #[Assert\Length(max: 255)]
     public string $name;
 
-    #[JMS\Type(name: 'string')]
     #[Assert\Type(type: 'string')]
     public ?string $description = null;
 
-    #[JMS\Type(name: 'strict_boolean')]
     #[Assert\NotNull]
     #[Assert\Type(type: 'boolean')]
     public bool $isSellable = false;
 
-    #[JMS\Type(name: 'integer')]
     #[Assert\NotNull]
     #[Assert\Type(type: 'integer')]
     public int $stock;
 
-    #[JMS\Type(name: 'App\Contract\Request\BroadcastListener\Common\Price')]
     #[Assert\Type(type: 'App\Contract\Request\BroadcastListener\Common\Price')]
     #[Assert\NotNull]
     #[Assert\Valid]
     public ListPrice $listPrice;
 
-    #[JMS\Type(name: 'datetime')]
     #[Assert\Type(type: 'datetime')]
     public ?\DateTime $updatedAt = null;
 
-    /**
-     * @codeCoverageIgnore
-     */
     public function getContext(): array
     {
         return [
--- src/Request/BroadcastListener/Product/ListPrice.php
+++ src/Request/BroadcastListener/Product/ListPrice.php
@@ -5,29 +5,23 @@ declare(strict_types=1);
 namespace Symfony\Sample\Request\BroadcastListener\Product;
 
 use Symfony\Sample\ContextualInterface;
-use JMS\Serializer\Annotation as JMS;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class ListPrice implements ContextualInterface
 {
-    #[JMS\Type(name: 'float')]
     #[Assert\Type(type: 'float')]
     #[Assert\NotBlank]
     public float $salePrice;
 
-    #[JMS\Type(name: 'float')]
     #[Assert\Type(type: 'float')]
     public ?float $specialPrice;
 
-    #[JMS\Type(name: 'datetime')]
     #[Assert\Type(type: 'datetime')]
     public ?\DateTime $specialFrom;
 
-    #[JMS\Type(name: 'datetime')]
     #[Assert\Type(type: 'datetime')]
     public ?\DateTime $specialTo;
 
-    #[JMS\Type(name: 'string')]
     #[Assert\NotBlank]
     #[Assert\Currency]
     public string $currencyCode;
