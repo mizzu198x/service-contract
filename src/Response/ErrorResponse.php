d2573c87a1c58c5da51682b29e9af8c37c3ecba4
Hung Nguyen <mizzu198x@gmail.com>
2024-05-01 02:26:14 +0700

    Add error response

---
+++ src/Response/ErrorResponse.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Sample\TC14\Response;
+
+class ErrorResponse
+{
+    public const DEFAULT_MESSAGE = 'General error';
+
+    public const DEFAULT_CODE = 1000000;
+
+    public string $message = self::DEFAULT_MESSAGE;
+
+    public int $code = self::DEFAULT_CODE;
+
+    public array $errorList = [];
+
+    public function toArray(): array
+    {
+        $errorList = [];
+        if (count($this->errorList) > 0) {
+            $errorList = ['errors' => $this->errorList];
+        }
+
+        return [
+            'error' => array_merge([
+                'message' => $this->message,
+                'code' => $this->code,
+            ], $errorList),
+        ];
+    }
+}
